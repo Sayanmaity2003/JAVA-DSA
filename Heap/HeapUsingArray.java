edbf2ac130d5f690794df031a9e627314888682d
Sayan Maity <sayanmaity2003@gmail.com>
2025-07-15 10:01:12 +0530

    HeapUsingArray-"Deletion in heap"

--- Heap/HeapUsingArray.java
+++ Heap/HeapUsingArray.java
@@ -45,6 +45,46 @@ public class HeapUsingArray{
             System.out.println();
         }
 
+        void delete(){
+            if(size==0){
+                System.out.println("Heap Underflow");
+                return;
+            }
+            System.out.println(arr[0]+" is deleted from the heap");
+            arr[0] = arr[size-1];
+            size--;
+
+            if(size==0){
+                return;
+            }else{
+                heapify(0);
+            }
+        }
+
+        private void heapify(int idx){
+            int root = idx;
+            int left = 2*idx + 1;
+            int right = 2*idx + 2;
+            int max = root;
+
+            if(left < size && arr[left] > arr[max]){
+                max = left;
+            }
+            if(right < size && arr[right] > arr[max]){
+                max = right;
+            }
+
+            if(max!=root){
+                int temp = arr[max];
+                arr[max] = arr[root];
+                arr[root] = temp;
+
+                heapify(max);
+            }
+
+
+        }
+        
     }
     public static void main(String args[]){
         maxHeap h1 = new maxHeap(6);
@@ -57,5 +97,8 @@ public class HeapUsingArray{
         h1.insert(24);
         h1.insert(1);
         h1.print();
+
+        h1.delete();
+        h1.delete();
     }
 }
